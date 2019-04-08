1ea42ac21d6b2758737e20f629cd9382c2f58012
lightzhu <957969881@qq.com>
2019-04-08 11:27:02 +0800

    增加滚筒抽奖

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Weapp
 微信小程序
+# 大转盘抽奖
+采用canvas绘制方法
+# 滚轮3d抽奖
+* 参考项目[https://github.com/zpzxgcr/choujiang](https://github.com/zpzxgcr/choujiang)
+* 对原始项目的数据结构进行相关改造
+* 基础库旋转1.9.90以上，否则会出现图片全都定位重叠在一起的问题
---
+++ pages/components/lucky-wheel.js
@@ -0,0 +1,144 @@
+Component({
+  options: {
+    addGlobalClass: true
+  },
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {},
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    redEnvelopeList: [{
+      list: [{
+        text: "一",
+        prizeImg: '../../images/red_envelope1.png',
+      }, {
+        text: "二",
+        prizeImg: '../../images/red_envelope2.png',
+      }, {
+        text: "三",
+        prizeImg: '../../images/red_envelope3.png'
+      }, {
+        text: "一",
+        prizeImg: '../../images/red_envelope1.png'
+      }, {
+        text: "五"
+      }, {
+        text: "六"
+      }, {
+        text: "七"
+      }, {
+        text: "八"
+      }, {
+        text: "九"
+      }, {
+        text: '十',
+        prize: true,
+      }]
+    }, 
+    {
+      list: [{
+        text: "一",
+        prizeImg: '../../images/red_envelope1.png'
+      }, {
+        text: "二",
+        prizeImg: '../../images/red_envelope2.png'
+      }, {
+        text: "三",
+        prizeImg: '../../images/red_envelope3.png'
+      }, {
+        text: "二",
+        prizeImg: '../../images/red_envelope2.png'
+      }, {
+        text: "五"
+      }, {
+        text: "六"
+      }, {
+        text: "七"
+      }, {
+        text: "八"
+      }, {
+        text: "九"
+      }, {
+        text: '十',
+        prize: true,
+      }]
+    },
+     {
+      list: [{
+        text: "一",
+        prizeImg: '../../images/red_envelope1.png'
+      }, {
+        text: "二",
+        prizeImg: '../../images/red_envelope2.png'
+      }, {
+        text: "三",
+        prizeImg: '../../images/red_envelope3.png'
+      }, {
+        text: "三",
+        prizeImg: '../../images/red_envelope3.png'
+      }, {
+        text: "五"
+      }, {
+        text: "六"
+      }, {
+        text: "七"
+      }, {
+        text: "八"
+      }, {
+        text: "九"
+      }, {
+        text: '十',
+        prize: true,
+      }]
+    }
+    ],
+    animation: -30,
+    time: 5,
+  },
+  attached: function() {
+    const that = this;
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    sort(data) {
+      //数组重排
+      return data.sort((a, b) => {
+        if (a.prize || b.prize) {
+
+        } else {
+          return a.text.charCodeAt() + parseInt(Math.random() * 1000) > b.text.charCodeAt() + parseInt(Math.random() * 1000)
+        }
+      })
+    },
+    /**
+     * @params start 抽奖事件
+     */
+    start() {
+      const that = this;
+      //  重置数组顺序后转动两圈
+      that.setData({
+        redEnvelopeList: [{
+          list: that.sort(that.data.redEnvelopeList[0].list)
+          },
+          {
+            list: that.sort(that.data.redEnvelopeList[1].list)
+          },
+          {
+            list: that.sort(that.data.redEnvelopeList[2].list)
+          }
+        ]
+      }, () => {
+        console.log(0);
+        that.setData({
+          animation: that.data.animation + 720
+        })
+      })
+    }
+  },
+})
